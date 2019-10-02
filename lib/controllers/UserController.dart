4671d4de61adfa9fbba9cbf5716e76db8ce78003
Marco Antonio <marco.ag.pegoraro@hotmail.com>
2019-10-01 23:41:09 -0300

    pequenas alteracoes no user

--- lib/controllers/UserController.dart
+++ lib/controllers/UserController.dart
@@ -1,3 +1,18 @@
+import 'package:todo/models/User.dart';
 import 'package:todo/todo.dart';
 
-class UserController extends ResourceController {}
+class UserController extends ResourceController {
+  UserController(this.context) {
+    acceptedContentTypes = [ContentType.json];
+  }
+
+  final ManagedContext context;
+
+  @Operation.post()
+  Future<Response> postUser() async {
+    final body = User()..read(await request.body.decode(), ignore: ["id"]);
+    final query = Query<User>(context)..values = body;
+    final toDo = await query.insert();
+    return Response.ok(toDo);
+  }
+}
